95d3492255e0816ef8894291fdee766da4ae7fea
kotet <kotet@users.noreply.github.com>
2016-11-26 10:20:44 +0900

    Bar

--- source/progress/bar.d
+++ source/progress/bar.d
@@ -1,2 +1,41 @@
 module progress.bar;
 import progress;
+import std.format;
+import std.string;
+import std.array;
+
+class Bar : Progress
+{
+    size_t width = 32;
+    string delegate() message;
+    string delegate() suffix;
+    string bar_prefix = " |";
+    string bar_suffix = "| ";
+    string empty_fill = " ";
+    string fill = "#";
+    bool hide_cursor = true;
+
+    this()
+    {
+        this.message = {return "";};
+        this.suffix = {return format("%s/%s",this.index,this.max);};
+        if(hide_cursor) file.write(HIDE_CURSOR);
+    }
+    override void update()
+    {
+        size_t filled_length = cast(size_t)(this.width * this.progress);
+        size_t empty_length = this.width - filled_length;
+        string message = this.message();
+        string bar = repeat(this.fill,filled_length);
+        string empty = repeat(this.empty_fill,empty_length);
+        string suffix = this.suffix();
+        string line = [
+            message,
+            this.bar_prefix,
+            bar,
+            empty,
+            this.bar_suffix,
+            suffix].join("");
+        file.write("\r\x1b[K",line);
+    }
+}
--- source/progress/package.d
+++ source/progress/package.d
@@ -3,3 +3,146 @@ module progress;
 public import progress.bar;
 public import progress.counter;
 public import progress.spinner;
+
+import std.stdio;
+import std.datetime;
+import std.algorithm;
+import std.concurrency;
+import std.math;
+
+immutable SHOW_CURSOR = "\x1b[?25h";
+immutable HIDE_CURSOR = "\x1b[?25l";
+
+class Infinite
+{
+    alias file = stderr;
+    size_t sma_window = 10;
+    size_t index;
+    StopWatch sw;
+    long ts;
+    long[] dt;
+    bool hide_cursor = false;
+
+    this()
+    {
+        this.index = 0;
+        this.sw.start();
+        ts = 0;
+    }
+
+    @property real avg()
+    {
+        return (dt.length == 0)?0:sum(dt) / dt.length / (10 ^^ 6);
+    }
+
+    @property real elapsed()
+    {
+        return sw.peek().usecs / (10 ^^ 6);
+    }
+
+    void update()
+    {
+        
+    }
+    void start()
+    {
+        
+    }
+    void finish()
+    {
+        if (hide_cursor)
+        {
+            file.write(SHOW_CURSOR);
+        }
+    }
+    void next(size_t n=1)
+    {
+        if (n > 0)
+        {
+            long now = sw.peek().usecs;
+            long _dt = (now - ts) / n;
+            this.dt = this.dt[($<sma_window)?0:$-sma_window+1 .. $] ~ _dt;
+            this.ts = now;
+        }
+        this.index += n;
+        this.update();
+    }
+    auto iter(T)(T[] it)
+    {
+        return new Generator!T(
+        {
+            foreach(x;it)
+            {
+                yield(x);
+                this.next();
+            }
+            this.finish();
+        }
+        );
+    }
+}
+
+class Progress : Infinite
+{
+    size_t max;
+    this(size_t max=100)
+    {
+        this.max = max;
+    }
+
+    @property long eta()
+    {
+        return cast(long)ceil(this.avg * this.remaining);
+    }
+    @property real percent()
+    {
+        return this.progress * 100;
+    }
+    @property real progress()
+    {
+        return min(1,cast(real)this.index / this.max);
+    }
+    @property real remaining()
+    {
+        return std.algorithm.comparison.max(this.max - this.index,0);
+    }
+    override void start()
+    {
+        this.update();
+    }
+    void goto_index(size_t index)
+    {
+        size_t incr = index - this.index;
+        this.next(incr);
+    }
+    auto iter(T)(T[] it)
+    {
+        try{
+            this.max = it.length;
+        }
+        catch (Exception e)
+        {
+            
+        }
+        return new Generator!T(
+        {
+            foreach(x;it)
+            {
+                yield(x);
+                this.next();
+            }
+            this.finish();
+        }
+        );
+    }
+}
+
+string repeat(string s,size_t n)
+{
+    string result;
+    foreach(i;0 .. n)
+    {
+        result ~= s;
+    }
+    return result;
+}
