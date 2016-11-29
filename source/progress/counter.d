a61c98ed371b13c41a52a492e5154dc879aab3c9
kotet <kotet@users.noreply.github.com>
2016-11-30 05:03:48 +0900

    Counter,Countdown,Stack,Pie

--- source/progress/counter.d
+++ source/progress/counter.d
@@ -1,2 +1,53 @@
 module progress.counter;
 import progress;
+
+import std.conv;
+import std.algorithm : min;
+
+class Counter : Infinite
+{
+    this()
+    {
+        this.hide_cursor = true;
+        super();
+    }
+    override void update()
+    {
+        this.write(this.index.to!string);
+    }
+}
+class Countdown : Progress
+{
+    this()
+    {
+        this.hide_cursor = true;
+        super();
+    }
+    override void update()
+    {
+        this.write(this.remaining.to!string);
+    }
+}
+class Stack : Progress
+{
+    string[] phases;
+    this()
+    {
+        this.hide_cursor = true;
+        this.phases = [" ", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"];
+        super();
+    }
+    override void update()
+    {
+        size_t nphases = this.phases.length;
+        size_t index = min(nphases-1,cast(size_t)(this.progress * nphases));
+        this.write(this.phases[index]);
+    }
+}
+class Pie : Stack
+{
+    this()
+    {
+        this.phases = ["○", "◔", "◑", "◕", "●"];
+    }
+}
