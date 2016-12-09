ec5f66148991c2b3c2e79e38c8526e645eeb8da2
kotet <kotet@users.noreply.github.com>
2016-12-09 15:20:22 +0900

    Add example spinner

---
+++ examples/spinner/source/app.d
@@ -0,0 +1,47 @@
+import core.thread : Thread,dur;
+import std.string : format;
+import progress.spinner;
+
+void main()
+{
+    {
+        Spinner s = new Spinner();
+        foreach(i;0 .. 100)
+        {
+            Thread.sleep(dur!("msecs")(100));
+            s.message = {return format("Basic spinner %s/100:",i+1);};
+            s.next();
+        }
+        s.finish();
+    }
+    {
+        PieSpinner ps = new PieSpinner();
+        foreach(i;0 .. 100)
+        {
+            Thread.sleep(dur!("msecs")(100));
+            ps.message = {return format("Pie spinner %s/100:",i+1);};
+            ps.next();
+        }
+        ps.finish();
+    }
+    {
+        MoonSpinner ms = new MoonSpinner();
+        foreach(i;0 .. 100)
+        {
+            Thread.sleep(dur!("msecs")(100));
+            ms.message = {return format("Moon spinner %s/100:",i+1);};
+            ms.next();
+        }
+        ms.finish();
+    }
+    {
+        LineSpinner ls = new LineSpinner();
+        foreach(i;0 .. 100)
+        {
+            Thread.sleep(dur!("msecs")(100));
+            ls.message = {return format("Line spinner %s/100:",i+1);};
+            ls.next();
+        }
+        ls.finish();
+    }
+}
